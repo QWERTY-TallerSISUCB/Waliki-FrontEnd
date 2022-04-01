0a75531c2016d3ee7668cb1f5aefc4703f2a7826
Sergio Troche <sergio.troche@ucb.edu.bo>
2022-04-01 18:27:29 -0400

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path:"", component:HomeComponent },
+  { path:"product-single", component:ProductsingleComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
