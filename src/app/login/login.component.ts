2bc7e983145e1fdff4e5b82ee0dd64890cb23cba
Sergio Troche <seriogionis@gmail.com>
2022-04-04 13:29:17 -0400

    [DEV] Pruebas de ingreso del login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,5 +13,40 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  ingresar(){
+      const usuario = this.form.value.usuario;
+      const password = this.form.value.password;
+
+      console.log(usuario);
+      console.log(password);
+
+      if(usuario == 'sergio' && password == '12345'){
+        //redireccionamos al dashboard
+        this.fakeLoading();
+      } else{
+        //mensaje de error
+        this.error();
+        this.form.reset();
+      }
+    }
+
+    error(){
+      this._snackBar.open('Usuario o contraseña incorrectos', '',{
+        duration: 5000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom'
+      })
+    }
+
+    fakeLoading(){
+      this.loading = true;
+      setTimeout(() => {
+
+        //redireccionamos al home
+
+        this.router.navigate(['home'])
+      }, 1500);
+    }
 
 }
