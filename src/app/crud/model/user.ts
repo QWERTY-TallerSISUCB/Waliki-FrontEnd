6ffdb9085ae75f9569d9beb9abad2cbeae80ba0c
Serius69 <seriogionis@gmail.com>
2022-04-18 23:55:29 -0400

    Borrado

--- src/app/crud/model/user.ts
+++
@@ -1,8 +0,0 @@
-// tiene que ser igual al dto del backend
-export class User {
-    email!: string;
-    //userId: number;
-    userName!: string;
-    userPassword!: string;
-    rol!: string;
-  }
