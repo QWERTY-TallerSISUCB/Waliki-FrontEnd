b1990dad79ca2e14f589bc503a9738422412aef7
Serius69 <seriogionis@gmail.com>
2022-05-05 12:47:56 -0400

    okta

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
 
     oidc: {
-        clientId: '<<UPDATE-WITH-YOUR-APP-CLIENT-ID>>',
-        issuer: 'https://<<UPDATE-WITH-YOUR-DEV-DOMAIN>>/oauth2/default',
+        clientId: '0oa4xew604FEoRhYj5d7',
+        issuer: 'https://dev-63917137.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
